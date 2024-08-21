5c3f8030000dfd1be131293aaf4ff9f8f5a22dc8
Camila Fabre <camilafabre98@gmail.com>
2024-08-21 13:17:59 -0300

    seccion catalogo

--- front/src/components/products/ProductCard.jsx
+++ front/src/components/products/ProductCard.jsx
@@ -1,8 +1,12 @@
-const ProductCard = ({title, url, img}) => {
-    return (
-        <>
-        </>
-    )
-}
+import styles from "../products/ProductCard.module.css";
+
+const ProductCard = ({ title, img }) => {
+  return (
+    <div className={styles.cardContainer}>
+      <img src={img} className={styles.imgCard}></img>
+      <span className={styles.titleCard}>{title}</span>
+    </div>
+  );
+};
 
 export default ProductCard;
--- front/src/components/products/Products.jsx
+++ front/src/components/products/Products.jsx
@@ -1,48 +1,59 @@
 import styles from "../products/Products.module.css";
+import ProductCard from "./ProductCard";
 
 const Products = () => {
   return (
-    <section className={styles.container}>
-      <h2 className={styles.title}>NUESTRO MENÚ</h2>
-      <div className={styles.scrollDownIndicator}>
-        <span className={styles.arrow}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            fill="currentColor"
-            class="bi bi-caret-down-fill"
-            viewBox="0 0 16 16"
-          >
-            <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
-          </svg>
-        </span>
-        <span className={styles.arrow}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            fill="currentColor"
-            class="bi bi-caret-down-fill"
-            viewBox="0 0 16 16"
-          >
-            <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
-          </svg>
-        </span>
-        <span className={styles.arrow}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            fill="currentColor"
-            class="bi bi-caret-down-fill"
-            viewBox="0 0 16 16"
-          >
-            <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
-          </svg>
-        </span>
+    <>
+      <div className={styles.container}>
+        <h2 className={styles.title}>NUESTRO MENÚ</h2>
+        <div className={styles.scrollDownIndicator}>
+          <span className={styles.arrow}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              class="bi bi-caret-down-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+            </svg>
+          </span>
+          <span className={styles.arrow}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              class="bi bi-caret-down-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+            </svg>
+          </span>
+          <span className={styles.arrow}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              class="bi bi-caret-down-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+            </svg>
+          </span>
+        </div>
       </div>
-    </section>
+      <div className={styles.container}>
+        <div className={styles.containerCards}>
+          <ProductCard title="Rolls" img="/products/1.png" />
+          <ProductCard title="Niguiris" img="/products/2.png" />
+          <ProductCard title="Combinados" img="/products/3.png" />
+          <ProductCard title="Extras" img="/products/4.png" />
+        </div>
+      </div>
+    </>
   );
 };
 
