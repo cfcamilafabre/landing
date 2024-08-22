65bc1e173eb5429c707eebf1a3fcf200319a6fab
Camila Fabre <camilafabre98@gmail.com>
2024-08-22 13:12:29 -0300

    detalles errorpage

--- front/src/components/errorPage/Error.jsx
+++ front/src/components/errorPage/Error.jsx
@@ -1,10 +1,10 @@
 const Error = () => {
   return (
     <>
-      <div style={{ textAlign: "center", marginTop: "30em" }}>
+      <div style={{ textAlign: "center", color:"#222222", backgroundColor:"#f5f5f5", gap:"3em", display:"flex", height:"50vw", flexDirection:"column", justifyContent:"Center", alignItems:"center"}}>
         <h1>404 - Página No Encontrada</h1>
         <p>Lo sentimos, la página que buscas no existe.</p>
-        <a to="/">Volver a la página principal</a>
+        <a href="/" style={{backgroundColor:"#222222", padding:"1em", width:"15em", }}>Volver a la página principal</a>
       </div>
     </>
   );
